07af3d3ea98b7b7b010409f1eb9c66cb68d8f09c
ankush807034 <parmar807034@gmail.com>
2025-06-20 11:21:34 +0530

    Added indobot.php

---
+++ indobot.php
@@ -0,0 +1,8 @@
+<?php
+echo "Tᴏ Cᴏɴɴᴇᴄᴛ ᴀ Bᴏᴛ, Yᴏᴜ Sʜᴏᴜʟᴅ Fᴏʟʟᴏᴡ Tʜᴇsᴇ Tᴡᴏ Sᴛᴇᴘs :\n\n";
+echo "1. Oᴘᴇɴ @BotFather Cʀᴇᴀᴛᴇ ᴀ Nᴇᴡ Bᴏᴛ\n";
+echo "2. Yᴏᴜ'ʟʟ Gᴇᴛ ᴀ Tᴏᴋᴇɴ (ᴇ.ɢ. 12345:6789ABCDEF) —  Jᴜsᴛ Cᴏᴘʏ Pᴀsᴛᴇ Iᴛ Tᴏ Tʜɪs Cʜᴀᴛ\n\n";
+echo "Example\n\n";
+echo "<pre>/mybot xxxxxx</pre>\n\n";
+echo "⚠️Wᴀʀɴɪɴɢ Dᴏɴ'ᴛ Cᴏɴɴᴇᴄᴛ ᴛʜᴇ ʙᴏᴛ ᴛʜᴀᴛ ᴀʟʀᴇᴀᴅʏ ᴄᴏɴɴᴇᴄᴛᴇᴅ";
+?>
